98827f0b492479e3c6f0db6b5fc89898e3cf90b6
Cristian Perafan <1110282139@u.icesi.edu.co>
2023-11-10 11:44:21 -0500

    Feat(Auth): Add login functionality and login time check

--- Auth/views.py
+++ Auth/views.py
@@ -1,12 +1,15 @@
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework import status
 from rest_framework.response import Response
-
-from .serializers import UserSerializer
 from rest_framework.authtoken.models import Token
+from rest_framework.decorators import authentication_classes,permission_classes
+from rest_framework.authentication import SessionAuthentication,TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
-
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
 from drf_yasg.utils import swagger_auto_schema
+from .serializers import UserSerializer
 
 
 # Create your views here.
@@ -39,5 +42,19 @@ def register(request):
     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
+def login(request):
+    user = get_object_or_404(User,username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response({'detail':'Not found'},status=status.HTTP_400_BAD_REQUEST)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+    return Response({'token': token.key})
+
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication,TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def test_token(request):
-    return Response({''})
+    token = Token.objects.get(user=request.user)
+    token_age = timezone.now() - token.created
+    return Response({'detail':'Token is valid', 'token_age': str(token_age)})
