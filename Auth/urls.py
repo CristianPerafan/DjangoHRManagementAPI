9e05beb4a1a07dcb038bbc1e0dae328221681ea4
Cristian Perafan <1110282139@u.icesi.edu.co>
2023-11-27 15:32:24 -0500

    Feat(Auth): Add custom user and update auth user model to my new model

--- Auth/urls.py
+++ Auth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.register,name='register'),
+    path('registerEmployee', views.registerEmployee,name='registerEmployee'),
     path('login', views.login,name='login'),
     path('testtoken', views.test_token,name='test_token'),
     path('logout', views.logout,name='logout'),
